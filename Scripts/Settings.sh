429a4b1330575f18d512e71fd7ff975aa1666500
yehualiu1979 <braddockkins@gmail.com>
2025-06-04 01:27:20 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -87,6 +87,8 @@ echo "CONFIG_PACKAGE_luci-app-vsftpd=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-watchcat=y" >> ./.config
 # 下载工具
 echo "CONFIG_PACKAGE_luci-app-aria2=y" >> ./.config
+# 简单的广告拦截
+echo "CONFIG_PACKAGE_luci-app-simple-adblock" >> ./.config
 # 强大的工具(需要添加源或git clone)
 # echo "CONFIG_PACKAGE_luci-app-lucky=y" >> ./.config
 # 网络通信工具
