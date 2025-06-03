cdd06117127f1886e1cc60248e69fb859b8f71ff
yehualiu1979 <braddockkins@gmail.com>
2025-06-04 01:23:47 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -81,6 +81,8 @@ echo "CONFIG_PACKAGE_jq=y" >> ./.config
 echo "CONFIG_PACKAGE_btop=y" >> ./.config
 # 多网盘存储
 echo "CONFIG_PACKAGE_luci-app-alist=y" >> ./.config
+# ftp工具
+echo "CONFIG_PACKAGE_luci-app-vsftpd=y" >> ./.config
 # 下载工具
 echo "CONFIG_PACKAGE_luci-app-aria2=y" >> ./.config
 # 强大的工具(需要添加源或git clone)
