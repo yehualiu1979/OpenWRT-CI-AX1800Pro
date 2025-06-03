223e212d15c7848691daa74e3c85692c0774bb71
yehualiu1979 <braddockkins@gmail.com>
2025-06-04 01:19:41 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -81,6 +81,8 @@ echo "CONFIG_PACKAGE_jq=y" >> ./.config
 echo "CONFIG_PACKAGE_btop=y" >> ./.config
 # 多网盘存储
 echo "CONFIG_PACKAGE_luci-app-alist=y" >> ./.config
+# 下载工具
+echo "CONFIG_PACKAGE_luci-app-aria2=y" >> ./.config
 # 强大的工具(需要添加源或git clone)
 # echo "CONFIG_PACKAGE_luci-app-lucky=y" >> ./.config
 # 网络通信工具
