ae9d2583fd626b9b50bacac5aa73f65c51fc8fe0
yehualiu1979 <braddockkins@gmail.com>
2026-03-26 05:14:45 +0800

    Add files via upload

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -56,7 +56,7 @@ UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main" "pkg"
 
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
-UPDATE_PACKAGE "alist" "sbwml/luci-app-alist" "main"
+# UPDATE_PACKAGE "alist" "sbwml/luci-app-alist" "main"
 UPDATE_PACKAGE "easytier" "EasyTier/luci-app-easytier" "main"
 UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
 UPDATE_PACKAGE "mosdns" "sbwml/luci-app-mosdns" "v5" "" "v2dat"
